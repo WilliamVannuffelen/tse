19d62977dec6bffb167334c343ab611cddc06154
William Vannuffelen <williamvannuffelen@gmail.com>
2024-10-24 21:42:14 +0200

    chore: move initconfig to main

--- config/config.go
+++ config/config.go
@@ -7,15 +7,23 @@ import (
 )
 
 type Config struct {
+	General GeneralConfig `mapstructure:"General"`
 	File    FileConfig    `mapstructure:"File"`
 	Project ProjectConfig `mapstructure:"Project"`
 	JiraRef JiraRefConfig `mapstructure:"JiraRef"`
 	AppRef  AppRefConfig  `mapstructure:"AppRef"`
 }
 
+type GeneralConfig struct {
+	DebugEnabled bool `mapstructure:"debugEnabled"`
+	LogLevel 	 string `mapstructure:"logLevel"`
+	SilenceConfigWarnings bool `mapstructure:"silenceConfigWarnings"`
+}
+
 type FileConfig struct {
 	TargetFilePath  string `mapstructure:"targetFilePath"`
 	TargetSheetName string `mapstructure:"targetSheetName"`
+	TemplateSheetName string `mapstructure:"templateSheetName"`
 }
 
 type ProjectConfig struct {
@@ -34,7 +42,7 @@ type AppRefConfig struct {
 
 var cfgFile string
 
-func InitConfig(debugEnabled bool) {
+func InitConfig() Config {
 	var config Config
 
 	log.SetOutput(os.Stdout)
@@ -49,18 +57,18 @@ func InitConfig(debugEnabled bool) {
 	}
 
 	if err := viper.ReadInConfig(); err == nil {
-		log.Println("Using config file:", viper.ConfigFileUsed())
+		//log.Println("Using config file:", viper.ConfigFileUsed())
 	} else {
 		log.Println("Failed to read config file:", err)
-	}
-
-	if debugEnabled {
-		log.Println("Raw config data:", viper.AllSettings())
 	}
 
 	err := viper.Unmarshal(&config)
 	if err != nil {
 		log.Println("Failed to unmarshal config:", err)
+	}
+	debugEnabled := config.General.DebugEnabled
+	if debugEnabled {
+		log.Println("Running in debug mode. Will print verbose messages.")
 	}
 	if debugEnabled {
 		log.Println("Unmarshalled config file.")
@@ -69,19 +77,28 @@ func InitConfig(debugEnabled bool) {
 		log.Printf("Config: %+v\n", config)
 	}
 
-	if config.File.TargetFilePath == "" {
-		log.Println("Warning: TargetFilePath is empty")
+	if !config.General.SilenceConfigWarnings {
+		if config.General.LogLevel == "" {
+			log.Println("Warning: LogLevel is empty. Will use 'info'.")
+		}
+		if config.File.TargetFilePath == "" {
+			log.Println("Warning: TargetFilePath is empty")
+		}
+		if config.File.TargetSheetName == "" {
+			log.Println("Warning: TargetSheetName is empty")
+		}
+		if config.File.TemplateSheetName == "" {
+			log.Println("Warning: TemplateSheetName is empty. Will use sheet at index 0.")
+		}
+		if config.Project.DefaultProjectName == "" {
+			log.Println("Warning: DefaultProjectName is empty")
+		}
+		if config.JiraRef.DefaultValue == "" {
+			log.Println("Warning: JiraRef DefaultValue is empty")
+		}
+		if config.AppRef.DefaultValue == "" {
+			log.Println("Warning: AppRef DefaultValue is empty")
+		}
 	}
-	if config.File.TargetSheetName == "" {
-		log.Println("Warning: TargetSheetName is empty")
-	}
-	if config.Project.DefaultProjectName == "" {
-		log.Println("Warning: DefaultProjectName is empty")
-	}
-	if config.JiraRef.DefaultValue == "" {
-		log.Println("Warning: JiraRef DefaultValue is empty")
-	}
-	if config.AppRef.DefaultValue == "" {
-		log.Println("Warning: AppRef DefaultValue is empty")
-	}
+	return config
 }
--- main.go
+++ main.go
@@ -5,21 +5,32 @@ package main
 
 import (
 	logger "github.com/williamvannuffelen/go_zaplogger_iso8601"
-	"github.com/williamvannuffelen/tse/cmd"
-	"github.com/williamvannuffelen/tse/workitem"
+	_ "github.com/williamvannuffelen/tse/cmd"
+	_ "github.com/williamvannuffelen/tse/workitem"
+	"github.com/williamvannuffelen/tse/excel"
+	"github.com/williamvannuffelen/tse/config"
 )
 
 // UNUSED allows unused variables to be included in Go programs
 func UNUSED(x ...interface{}) {}
 
 func main() {
-	log, err := logger.InitLogger("log.txt", "debug")
+	config := config.InitConfig()
+	log, err := logger.InitLogger("log.txt", config.General.LogLevel)
 	if err != nil {
 		log.Warn(err)
 	}
 	log.Info("Foo!")
-	cmd.SetLogger(log)
-	cmd.Execute()
-	workitem.SetLogger(log)
-	workitem.Run()
+	log.Info(config.General.DebugEnabled)
+	//cmd.SetLogger(log)
+	//cmd.Execute()
+	//workitem.SetLogger(log)
+	//workitem.Run()
+	excel.SetLogger(log)
+	err = excel.MakeSheet("t_upload2.xlsx", "2024-10-21", "")
+	if err != nil {
+		log.Warn(err)
+	} else {
+		log.Info("Sheet created")
+	}
 }
